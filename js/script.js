800a5f59657416278ce73bd422330fed8109b0cd
Shawn Park <spark0517@Shawns-MacBook-Pro.local>
2021-09-15 13:58:59 -0700

    Update 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // Variables
 let pl1El = document.querySelector("#pl1");
 let pl2El = document.querySelector("#pl2");
-let gameboardEl = document.querySelector("#gameboard");
 let playAgainEl = document.querySelector("#play-again");
 let playAgainBtnEl = document.querySelector("#play-again-btn");
 let game = {};
+let player;
+let totalTurns = 0;
+const gameboardEl = document.querySelector(".gameboard");
+const piece = document.getElementsByClassName("piece");
 
 
 // Messages
@@ -20,22 +23,55 @@ game.setup = {
 
 
 let gameboard = [
-  [null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null],
-]
+  [0, 0, 0, 0, 0, 0],  // Column 0
+  [0, 0, 0, 0, 0, 0],  // Column 1
+  [0, 0, 0, 0, 0, 0],  // Column 2
+  [0, 0, 0, 0, 0, 0],  // Column 3
+  [0, 0, 0, 0, 0, 0],  // Column 4
+  [0, 0, 0, 0, 0, 0],  // Column 5
+  [0, 0, 0, 0, 0, 0],  // Column 6
+  ];
 
 // Event Listeners
 playAgainBtnEl.addEventListener('click', () => location.reload());
-gameboardEl.addEventListener("click", null);
+
+gameboardEl.addEventListener("click", function(e){
+  changeColor(e)
+})
+
 
 // Functions
 
+function checkPlayerTurn(){
+  return player = totalTurns % 2
+}
+
+function changeColor(e){
+    console.log(e.target, 'check for divs')
+    console.log(e.target.tagName)
+
+    if (e.target.tagName === 'DIV' && player ){
+      e.target.style.backgroundColor = 'red';
+      totalTurns++
+    } 
+    else if (e.target.tagName === 'DIV' && !player ){
+      e.target.style.backgroundColor = 'yellow';
+      totalTurns++
+    }
+    
+    checkPlayerTurn()
+}
+//Make condition so colored circle can not change colors.
 
 // Game piece falls to bottom
+/*
+- Check circle underneath to see if white or colored.
+  - if white, check circle below that
+  - if colored, stay
+  - repeat until bottom. if bottom, stay.
 
-//Check for winer
 
+*/
+
+
+//Check for winner
